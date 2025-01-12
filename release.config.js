4069d9a2c1269cc46e4d3d55db397dc644134981
satria <satria.gitu4@gmail.com>
2025-01-13 01:44:36 +0700

    fix: add semantic-release configuration config

--- release.config.js
+++ release.config.js
@@ -1,16 +1,16 @@
-{
-  "branches"; ["main", "staging", "dev"],
-  "plugins"; [
+module.exports = {
+  branches: ["main", "staging", "dev"],
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     [
       "@semantic-release/github",
       {
-        "assets": [
-          {"path": "dist/*.zip", "label": "Build Package"}
+        assets: [
+          { path: "dist/*.zip", label: "Build Package" }
         ]
       }
     ]
   ]
-}
+};
